53cddcfe00cee766cf7c11a9fb88ae116118155b
Miguel A. Zapata <miguel.zapatae@gmail.com>
2015-04-23 18:58:03 -0300

    added form and plugins to send mails

--- www/js/controllers.js
+++ www/js/controllers.js
@@ -1,20 +1,41 @@
 angular.module('starter.controllers', [])
 
-.controller('DashCtrl', function($scope) {})
+.controller('FormController', function($scope, $ionicPopup, $timeout) {
 
-.controller('ChatsCtrl', function($scope, Chats) {
-  $scope.chats = Chats.all();
-  $scope.remove = function(chat) {
-    Chats.remove(chat);
-  }
-})
+  // Triggered on a button click, or some other target
+  $scope.showPopup = function() {
 
-.controller('ChatDetailCtrl', function($scope, $stateParams, Chats) {
-  $scope.chat = Chats.get($stateParams.chatId);
-})
-
-.controller('AccountCtrl', function($scope) {
-  $scope.settings = {
-    enableFriends: true
+    var alertPopup = $ionicPopup.alert({
+      title: 'Por favor...',
+      template: 'Debe incluir su nombre en la peticion'
+    });
+    alertPopup.then(function(res) {
+      console.log('Thank you for not eating my delicious ice cream cone');
+    });
   };
-});
+
+  $scope.sendMail= function(username) {
+    $scope.content = "Estimados miembros de la comisión de Constitución, el motivo de este mail es para pedirle que apoye el proyecto de acuerdo para poner límite a la dieta parlamentaria, no olvide que somos nosotros, el pueblo, los que deberíamos decidir en democracia que es lo que queremos para nuestros país. Saluda atte, " + username;
+
+    if (angular.isUndefined(username) || username === null) {
+      $scope.showPopup();
+    } else {
+      if(window.plugins && window.plugins.emailComposer) {
+        window.plugins.emailComposer.showEmailComposerWithCallback(function(result) {
+          console.log("Response -> " + result);
+        }, 
+        "Peticion para apoyar Ley de Dieta", // Subject
+        $scope.content,           // Body
+        ['oandrade@congreso.cl','gceroni@congreso.cl','fchain@congreso.cl','juanantoniocoloma@congreso.cl','cmonckeberg@congreso.cl','asquella@congreso.cl','renzo.trisotti@congreso.cl'],    // To
+        null,                         // CC
+        null,                         // BCC
+        false,                        // isHTML
+        null,                         // Attachments
+        null);                        // Attachment Data
+        }
+      }
+    }
+  }
+);
+
+
